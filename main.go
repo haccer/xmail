b7b26f8463dc41e6dc8551e28f67db1f037d5806
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2024-05-14 22:45:46 -0400

    Make it faster

--- main.go
+++ main.go
@@ -5,23 +5,45 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/haccer/available"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	uniqueDomains := make(map[string]bool)
+
+	var wg sync.WaitGroup
+	workerCount := 100
+	jobs := make(chan string, workerCount)
+
+	for w := 1; w <= workerCount; w++ {
+		go func() {
+			for domain := range jobs {
+				available := available.Domain(domain)
+				if available {
+					fmt.Printf("[+] Available Domain: %s\n", domain)
+				}
+				wg.Done()
+			}
+		}()
+	}
 
 	for scanner.Scan() {
 		email := scanner.Text()
 		at := strings.LastIndex(email, "@")
 		if at >= 0 {
 			domain := email[at+1:]
-
-			available := available.Domain(domain)
-			if available {
-				fmt.Printf("[+] Available Domain: %s | Email: %s\n", domain, email)
-			}
+			uniqueDomains[domain] = true
 		}
 	}
+
+	for domain := range uniqueDomains {
+		wg.Add(1)
+		jobs <- domain
+	}
+	close(jobs)
+
+	wg.Wait()
 }
